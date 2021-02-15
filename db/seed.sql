ecc03900c2bd74735b14eef6d68220bf3c192263
Kyle G <legokyle@frontier.com>
2021-02-15 15:14:25 -0800

    Added seed.sql file and inserted code to both files in db

---
+++ db/seed.sql
@@ -0,0 +1,10 @@
+USE burgers_db;
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('cheeseburger', 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('whopper', 0);
+
+INSERT INTO burgers(burger_name, devoured)
+VALUES ('double patty bacon cheeseburger', 1);
